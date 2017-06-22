422a04af9a7d8a693fdb4c021eb633368b82cd63
Rick Selby <rick@selby-family.co.uk>
2017-06-22 10:45:41 +0100

    Fix facade

--- src/Facades/Html.php
+++ src/Facades/Html.php
@@ -2,9 +2,10 @@
 
 namespace Spatie\Html\Facades;
 
+use Illuminate\Support\Facades\Facade;
 use Spatie\Html\Html as HtmlBuilder;
 
-class Html
+class Html extends Facade
 {
     /**
      * Get the registered name of the component.

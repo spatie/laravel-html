3693d4755f4008877434a02f3dd929a2a2fff82d
Lucas Pires <lucas.pires.mattos@gmail.com>
2017-10-13 23:57:55 -0300

    input: improve tests

---
+++ tests/Html/FileTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Spatie\Html\Test\Html;
+
+class FileTest extends TestCase
+{
+    /** @test */
+    public function it_can_create_a_file_input()
+    {
+        $this->assertHtmlStringEqualsHtmlString(
+            '<input type="file">',
+            $this->html->file()
+        );
+    }
+
+    /** @test */
+    public function it_can_create_a_file_input_with_a_name()
+    {
+        $this->assertHtmlStringEqualsHtmlString(
+            '<input id="archives" type="file" name="archives">',
+            $this->html->file('archives')
+        );
+    }
+}
---
+++ tests/Html/TelTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Spatie\Html\Test\Html;
+
+class TelTest extends TestCase
+{
+    /** @test */
+    public function it_can_create_a_tel_link()
+    {
+        $this->assertHtmlStringEqualsHtmlString(
+            '<a href="tel:+19999999999"></a>',
+            $this->html->tel('+19999999999')
+        );
+    }
+
+    /** @test */
+    public function it_can_create_a_tel_link_with_contents()
+    {
+        $this->assertHtmlStringEqualsHtmlString(
+            '<a href="tel:+19999999999">Call me</a>',
+            $this->html->tel('+19999999999', 'Call me')
+        );
+    }
+}

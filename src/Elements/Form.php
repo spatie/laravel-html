e466d8b4f76120aef711b507f532f43841c18af6
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-02-20 15:25:33 +0100

    Fix novalidate

--- src/Elements/Form.php
+++ src/Elements/Form.php
@@ -31,7 +31,7 @@ class Form extends BaseElement
     /**
      * @return static
      */
-    public function novalidate($method)
+    public function novalidate()
     {
         return $this->attribute('novalidate');
     }

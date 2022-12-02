dcf05f3e4297d2dcdbce12204b0e75eb4d4e1da9
Stefan Damjanovic <>
2022-12-02 19:41:31 +0100

    Add p element

---
+++ tests/Elements/PTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Spatie\Html\Test\Elements;
+
+use Spatie\Html\Elements\P;
+use Spatie\Html\Test\TestCase;
+
+class PTest extends TestCase
+{
+    /** @test */
+    public function it_can_create_an_p_element()
+    {
+        $this->assertHtmlStringEqualsHtmlString(
+            '<p></p>',
+            P::create()
+        );
+    }
+}

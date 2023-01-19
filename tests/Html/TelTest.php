3a7d3b1b36482e0ef2fa420bcf07f5bae31f4541
Alex Manase <malexandru699@gmail.com>
2023-01-19 17:21:58 +0200

    refactor: TelTest

--- tests/Html/TelTest.php
+++ tests/Html/TelTest.php
@@ -1,24 +1,15 @@
 <?php
 
-namespace Spatie\Html\Test\Html;
+it('can create a tel link', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<a href="tel:+19999999999">+19999999999</a>',
+        $this->html->tel('+19999999999')
+    );
+});
 
-class TelTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_tel_link()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<a href="tel:+19999999999">+19999999999</a>',
-            $this->html->tel('+19999999999')
-        );
-    }
-
-    /** @test */
-    public function it_can_create_a_tel_link_with_contents()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<a href="tel:+19999999999">Call me</a>',
-            $this->html->tel('+19999999999', 'Call me')
-        );
-    }
-}
+it('can create a tel link with contents', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<a href="tel:+19999999999">Call me</a>',
+        $this->html->tel('+19999999999', 'Call me')
+    );
+});

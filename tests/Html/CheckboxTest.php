ab45aaccaefcc566dcff3c293eaf4860834625e0
Alex Manase <malexandru699@gmail.com>
2023-01-19 16:17:16 +0200

    refactor: CheckboxTest

--- tests/Html/CheckboxTest.php
+++ tests/Html/CheckboxTest.php
@@ -1,51 +1,36 @@
 <?php
 
-namespace Spatie\Html\Test\Html;
+it('can create a checkbox', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="checkbox" value="1">',
+        $this->html->checkbox()
+    );
+});
 
-class CheckboxTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_checkbox()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="checkbox" value="1">',
-            $this->html->checkbox()
-        );
-    }
+it('can create a checkbox with a name', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="checkbox" name="my_checkbox" id="my_checkbox" value="1">',
+        $this->html->checkbox('my_checkbox')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_checkbox_with_a_name()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="checkbox" name="my_checkbox" id="my_checkbox" value="1">',
-            $this->html->checkbox('my_checkbox')
-        );
-    }
+it('can create a checked checkbox with a name', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="checkbox" name="my_checkbox" id="my_checkbox" checked="checked" value="1">',
+        $this->html->checkbox('my_checkbox', true)
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_checked_checkbox_with_a_name()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="checkbox" name="my_checkbox" id="my_checkbox" checked="checked" value="1">',
-            $this->html->checkbox('my_checkbox', true)
-        );
-    }
+it('can create a checkbox with a name and a custom value', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="checkbox" name="my_checkbox" id="my_checkbox" checked="checked" value="foo">',
+        $this->html->checkbox('my_checkbox', true, 'foo')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_checkbox_with_a_name_and_a_custom_value()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="checkbox" name="my_checkbox" id="my_checkbox" checked="checked" value="foo">',
-            $this->html->checkbox('my_checkbox', true, 'foo')
-        );
-    }
-
-    /** @test */
-    public function it_can_create_a_checkbox_with_a_name_and_a_zero_value()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="checkbox" name="my_checkbox" id="my_checkbox" checked="checked" value="0">',
-            $this->html->checkbox('my_checkbox', true, 0)
-        );
-    }
-}
+it('can create a checkbox with a name and a zero value', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="checkbox" name="my_checkbox" id="my_checkbox" checked="checked" value="0">',
+        $this->html->checkbox('my_checkbox', true, 0)
+    );
+});

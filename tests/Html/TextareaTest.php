ea859488566e532d4cf817037fe2f9962ededf6c
Alex Manase <malexandru699@gmail.com>
2023-01-19 17:23:46 +0200

    refactor: TextareaTest

--- tests/Html/TextareaTest.php
+++ tests/Html/TextareaTest.php
@@ -1,33 +1,22 @@
 <?php
 
-namespace Spatie\Html\Test\Html;
+it('can create a textarea', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<textarea></textarea>',
+        $this->html->textarea()
+    );
+});
 
-class TextareaTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_textarea()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea></textarea>',
-            $this->html->textarea()
-        );
-    }
+it('can create a textarea with a name', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<textarea id="description" name="description"></textarea>',
+        $this->html->textarea('description')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_textarea_with_a_name()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea id="description" name="description"></textarea>',
-            $this->html->textarea('description')
-        );
-    }
-
-    /** @test */
-    public function it_can_create_a_textarea_with_a_value()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea id="description" name="description">Foo bar</textarea>',
-            $this->html->textarea('description', 'Foo bar')
-        );
-    }
-}
+it('can create a textarea with a value', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<textarea id="description" name="description">Foo bar</textarea>',
+        $this->html->textarea('description', 'Foo bar')
+    );
+});

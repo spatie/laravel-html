de45d70de1de3155720eba722eda6aa2d50afd77
Alex Manase <malexandru699@gmail.com>
2023-01-19 17:43:11 +0200

    refactor: ButtonTest

--- tests/Elements/ButtonTest.php
+++ tests/Elements/ButtonTest.php
@@ -1,63 +1,39 @@
 <?php
 
-namespace Spatie\Html\Test\Elements;
-
 use Spatie\Html\Elements\Button;
-use Spatie\Html\Test\TestCase;
 
-class ButtonTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_button()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<button></button>',
-            Button::create()
-        );
-    }
+it('can create a button')
+    ->assertHtmlStringEqualsHtmlString(
+        '<button></button>',
+        Button::create()
+    );;
 
-    /** @test */
-    public function it_can_create_a_button_with_a_type()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<button type="submit"></button>',
-            Button::create()->type('submit')
-        );
-    }
+it('can create a button with a type')
+    ->assertHtmlStringEqualsHtmlString(
+        '<button type="submit"></button>',
+        Button::create()->type('submit')
+    );;
 
-    /** @test */
-    public function it_can_create_a_button_with_a_value()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<button value="1"></button>',
-            Button::create()->value(1)
-        );
-    }
+it('can create a button with a value')
+    ->assertHtmlStringEqualsHtmlString(
+        '<button value="1"></button>',
+        Button::create()->value(1)
+    );;
 
-    /** @test */
-    public function it_can_create_a_button_with_a_name()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<button name="foo"></button>',
-            Button::create()->name('foo')
-        );
-    }
+it('can create a button with a name')
+    ->assertHtmlStringEqualsHtmlString(
+        '<button name="foo"></button>',
+        Button::create()->name('foo')
+    );
 
-    /** @test */
-    public function it_can_create_a_button_with_a_name_and_value()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<button name="foo" value="bar"></button>',
-            Button::create()->name('foo')->value('bar')
-        );
-    }
+it('can create a button with a name and value')
+    ->assertHtmlStringEqualsHtmlString(
+        '<button name="foo" value="bar"></button>',
+        Button::create()->name('foo')->value('bar')
+    );
 
-    /** @test */
-    public function it_can_disable_a_button()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<button disabled></button>',
-            Button::create()->disabled()
-        );
-    }
-}
+it('can disable a button')
+    ->assertHtmlStringEqualsHtmlString(
+        '<button disabled></button>',
+        Button::create()->disabled()
+    );

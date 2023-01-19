4a7e1e6b50b84beb608bac906bda2c991f6d1b55
Alex Manase <malexandru699@gmail.com>
2023-01-19 17:18:31 +0200

    refactor: SelectTest

--- tests/Html/SelectTest.php
+++ tests/Html/SelectTest.php
@@ -1,77 +1,62 @@
 <?php
 
-namespace Spatie\Html\Test\Html;
+it('can render a select element', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<select></select>',
+        $this->html->select()->render()
+    );
+});
 
-class SelectTest extends TestCase
-{
-    /** @test */
-    public function it_can_render_a_select_element()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select></select>',
-            $this->html->select()->render()
-        );
-    }
+it('can render a select element required', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<select required></select>',
+        $this->html->select()->required()->render()
+    );
+});
 
-    /** @test */
-    public function it_can_render_a_select_element_required()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select required></select>',
-            $this->html->select()->required()->render()
-        );
-    }
+it('can render a select element with options', function () {
+    $options = [
+        'value1' => 'text1',
+        'value2' => 'text2',
+    ];
 
-    /** @test */
-    public function it_can_render_a_select_element_with_options()
-    {
-        $options = [
-            'value1' => 'text1',
-            'value2' => 'text2',
-        ];
-
-        assertHtmlStringEqualsHtmlString(
-            '<select name="select" id="select">
+    assertHtmlStringEqualsHtmlString(
+        '<select name="select" id="select">
                 <option value="value1">text1</option>
                 <option value="value2">text2</option>
             </select>',
-            $this->html->select('select', $options)->render()
-        );
-    }
+        $this->html->select('select', $options)->render()
+    );
+});
 
-    /** @test */
-    public function it_can_render_a_select_element_with_options_with_a_selected_value()
-    {
-        $options = [
-            'value1' => 'text1',
-            'value2' => 'text2',
-        ];
+it('can render a select element with options with a selected value', function () {
+    $options = [
+        'value1' => 'text1',
+        'value2' => 'text2',
+    ];
 
-        assertHtmlStringEqualsHtmlString(
-            '<select name="select" id="select">
+    assertHtmlStringEqualsHtmlString(
+        '<select name="select" id="select">
                 <option value="value1" selected="selected">text1</option>
                 <option value="value2">text2</option>
             </select>',
-            $this->html->select('select', $options, 'value1')->render()
-        );
-    }
+        $this->html->select('select', $options, 'value1')->render()
+    );
+});
 
-    /** @test */
-    public function it_can_render_a_select_element_with_options_with_a_selected_value_when_the_values_are_similar()
-    {
-        $options = [
-            '0' => '0',
-            '2' => '2',
-            '+2' => '+2',
-        ];
+it('can render a select element with options with a selected value when the values are similar', function () {
+    $options = [
+        '0' => '0',
+        '2' => '2',
+        '+2' => '+2',
+    ];
 
-        assertHtmlStringEqualsHtmlString(
-            '<select name="select" id="select">
+    assertHtmlStringEqualsHtmlString(
+        '<select name="select" id="select">
                 <option value="0">0</option>
                 <option selected="selected" value="2">2</option>
                 <option value="+2" selected="selected">+2</option>
             </select>',
-            $this->html->select('select', $options, '+2')->render()
-        );
-    }
-}
+        $this->html->select('select', $options, '+2')->render()
+    );
+});

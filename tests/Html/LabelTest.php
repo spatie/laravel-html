f2f4ebb315b1c7348b0e2ec2356e320ad9cf5c26
Alex Manase <malexandru699@gmail.com>
2023-01-19 16:52:13 +0200

    refactor: LabelTest

--- tests/Html/LabelTest.php
+++ tests/Html/LabelTest.php
@@ -1,78 +1,57 @@
 <?php
 
-namespace Spatie\Html\Test\Html;
+it('can create a label', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<label></label>',
+        $this->html->label()
+    );
+});
 
-class LabelTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_label()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<label></label>',
-            $this->html->label()
-        );
-    }
+it('can create a label with contents', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<label>Hi</label>',
+        $this->html->label('Hi')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_label_with_contents()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<label>Hi</label>',
-            $this->html->label('Hi')
-        );
-    }
+it('can create a label with html contents', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<label><em>Hi</em></label>',
+        $this->html->label('<em>Hi</em>')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_label_with_html_contents()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<label><em>Hi</em></label>',
-            $this->html->label('<em>Hi</em>')
-        );
-    }
+it('can create a label with a custom for attribute', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<label for="some_input_id">Hi</label>',
+        $this->html->label('Hi', 'some_input_id')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_label_with_a_custom_for_attribute()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<label for="some_input_id">Hi</label>',
-            $this->html->label('Hi', 'some_input_id')
-        );
-    }
+it('can create a label with integer content', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<label>5000</label>',
+        $this->html->label(5000)
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_label_with_integer_content()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<label>5000</label>',
-            $this->html->label(5000)
-        );
-    }
+it('can create a label with float content', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<label>5000.5</label>',
+        $this->html->label(5000.5)
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_label_with_float_content()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<label>5000.5</label>',
-            $this->html->label(5000.5)
-        );
-    }
+it('can create a label with hexadecimal content', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<label>291</label>',
+        $this->html->label(0x123)
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_label_with_hexadecimal_content()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<label>291</label>',
-            $this->html->label(0x123)
-        );
-    }
-
-    /** @test */
-    public function it_can_create_a_label_with_octal_content()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<label>209</label>',
-            $this->html->label(0321)
-        );
-    }
-}
+it('can create a label with octal content', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<label>209</label>',
+        $this->html->label(0321)
+    );
+});

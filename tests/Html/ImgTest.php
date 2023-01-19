2bd7a74c862f2dedf38f14ad6f2ba3b9df6321a9
Alex Manase <malexandru699@gmail.com>
2023-01-19 16:36:40 +0200

    refactor: ImgTest and InputTest

--- tests/Html/ImgTest.php
+++ tests/Html/ImgTest.php
@@ -2,14 +2,9 @@
 
 namespace Spatie\Html\Test\Html;
 
-class ImgTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_img_tag_with_image_source_and_alt()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<img src="/path/to/image/file" alt="alt_value">',
-            $this->html->img('/path/to/image/file', 'alt_value')
-        );
-    }
-}
+it('can create a img tag with image source and alt', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<img src="/path/to/image/file" alt="alt_value">',
+        $this->html->img('/path/to/image/file', 'alt_value')
+    );
+});
--- tests/Html/InputTest.php
+++ tests/Html/InputTest.php
@@ -1,346 +1,269 @@
 <?php
 
-namespace Spatie\Html\Test\Html;
+it('can create an input', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input>',
+        $this->html->input()
+    );
+});
 
-class InputTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_an_input()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input>',
-            $this->html->input()
-        );
-    }
+it('can create an input with a custom type', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="text">',
+        $this->html->input('text')
+    );
+});
 
-    /** @test */
-    public function it_can_create_an_input_with_a_custom_type()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="text">',
-            $this->html->input('text')
-        );
-    }
+it('can create an input with a name', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="foo" type="text" name="foo">',
+        $this->html->input('text', 'foo')
+    );
+});
 
-    /** @test */
-    public function it_can_create_an_input_with_a_name()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="foo" type="text" name="foo">',
-            $this->html->input('text', 'foo')
-        );
-    }
+it('can create an input with a value', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="foo" type="text" name="foo" value="bar">',
+        $this->html->input('text', 'foo', 'bar')
+    );
+});
 
-    /** @test */
-    public function it_can_create_an_input_with_a_value()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="foo" type="text" name="foo" value="bar">',
-            $this->html->input('text', 'foo', 'bar')
-        );
-    }
+it('can create an input with a placeholder', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input placeholder="Foo bar">',
+        $this->html->input()->placeholder('Foo bar')
+    );
+});
 
-    /** @test */
-    public function it_can_create_an_input_with_a_placeholder()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input placeholder="Foo bar">',
-            $this->html->input()->placeholder('Foo bar')
-        );
-    }
+it('can create an input that is required', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input required>',
+        $this->html->input()->required()
+    );
+});
 
-    /** @test */
-    public function it_can_create_an_input_that_is_required()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input required>',
-            $this->html->input()->required()
-        );
-    }
+it('can create an input that has autofocus', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input autofocus>',
+        $this->html->input()->autofocus()
+    );
+});
 
-    /** @test */
-    public function it_can_create_an_input_that_has_autofocus()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input autofocus>',
-            $this->html->input()->autofocus()
-        );
-    }
+it('can check an input', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="checkbox" checked="checked">',
+        $this->html->input('checkbox')->checked(true)
+    );
 
-    /** @test */
-    public function it_can_check_an_input()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="checkbox" checked="checked">',
-            $this->html->input('checkbox')->checked(true)
-        );
+    assertHtmlStringEqualsHtmlString(
+        '<input type="checkbox" checked="checked">',
+        $this->html->input('checkbox')->checked(true)
+    );
+});
 
-        assertHtmlStringEqualsHtmlString(
-            '<input type="checkbox" checked="checked">',
-            $this->html->input('checkbox')->checked(true)
-        );
-    }
+it('can uncheck an input', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="checkbox">',
+        $this->html->input('checkbox')->checked()->checked(false)
+    );
 
-    /** @test */
-    public function it_can_uncheck_an_input()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="checkbox">',
-            $this->html->input('checkbox')->checked()->checked(false)
-        );
+    assertHtmlStringEqualsHtmlString(
+        '<input type="checkbox">',
+        $this->html->input('checkbox')->checked()->unchecked()
+    );
+});
 
-        assertHtmlStringEqualsHtmlString(
-            '<input type="checkbox">',
-            $this->html->input('checkbox')->checked()->unchecked()
-        );
-    }
+it('can create an input that is readonly', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input readonly>',
+        $this->html->input()->isReadonly()
+    );
+});
 
-    /** @test */
-    public function it_can_create_an_input_that_is_readonly()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input readonly>',
-            $this->html->input()->isReadonly()
-        );
-    }
+it('can create an input without readonly', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input>',
+        $this->html->input()->isReadonly(false)
+    );
+});
 
-    /** @test */
-    public function it_can_create_an_input_without_readonly()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input>',
-            $this->html->input()->isReadonly(false)
-        );
-    }
+it('can remove readonly from a readonly input', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input>',
+        $this->html->input()->isReadonly()->isReadonly(false)
+    );
+});
 
-    /** @test */
-    public function it_can_remove_readonly_from_a_readonly_input()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input>',
-            $this->html->input()->isReadonly()->isReadonly(false)
-        );
-    }
+it('can create a date input', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="date">',
+        $this->html->date()
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_date_input()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="date">',
-            $this->html->date()
-        );
-    }
+it('can create a date input with blank date', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_date" name="test_date" type="date" value=""/>',
+        $this->html->date('test_date', '')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_date_input_with_blank_date()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_date" name="test_date" type="date" value=""/>',
-            $this->html->date('test_date', '')
-        );
-    }
+it('can create a date input and format date', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_date" name="test_date" type="date" value="2017-09-04"/>',
+        $this->html->date('test_date', '2017-09-04T23:33:32')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_date_input_and_format_date()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_date" name="test_date" type="date" value="2017-09-04"/>',
-            $this->html->date('test_date', '2017-09-04T23:33:32')
-        );
-    }
+it('can create a date input and format model date', function () {
+    $this->html->model(['test_date' => '2017-09-04T23:33:32']);
 
-    /** @test */
-    public function it_can_create_a_date_input_and_format_model_date()
-    {
-        $this->html->model(['test_date' => '2017-09-04T23:33:32']);
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_date" name="test_date" type="date" value="2017-09-04"/>',
+        $this->html->date('test_date')
+    );
+});
 
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_date" name="test_date" type="date" value="2017-09-04"/>',
-            $this->html->date('test_date')
-        );
-    }
+it('can create a date input with invalid date', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_date" name="test_date" type="date" value="notadate"/>',
+        $this->html->date('test_date', 'notadate')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_date_input_with_invalid_date()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_date" name="test_date" type="date" value="notadate"/>',
-            $this->html->date('test_date', 'notadate')
-        );
-    }
+it('can create a datetime input', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="datetime-local">',
+        $this->html->datetime()
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_datetime_input()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="datetime-local">',
-            $this->html->datetime()
-        );
-    }
+it('can create a datetime input with blank date', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_datetime" name="test_datetime" type="datetime-local" value=""/>',
+        $this->html->datetime('test_datetime', '')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_datetime_input_with_blank_date()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_datetime" name="test_datetime" type="datetime-local" value=""/>',
-            $this->html->datetime('test_datetime', '')
-        );
-    }
+it('can create a datetime input and format date', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_datetime" name="test_datetime" type="datetime-local" value="2020-01-20T15:00:12"/>',
+        $this->html->datetime('test_datetime', '2020-01-20T15:00:12')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_datetime_input_and_format_date()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_datetime" name="test_datetime" type="datetime-local" value="2020-01-20T15:00:12"/>',
-            $this->html->datetime('test_datetime', '2020-01-20T15:00:12')
-        );
-    }
+it('can create a datetime input and format model date', function () {
+    $this->html->model(['test_datetime' => '2020-01-20T15:00:12']);
 
-    /** @test */
-    public function it_can_create_a_datetime_input_and_format_model_date()
-    {
-        $this->html->model(['test_datetime' => '2020-01-20T15:00:12']);
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_datetime" name="test_datetime" type="datetime-local" value="2020-01-20T15:00:12"/>',
+        $this->html->datetime('test_datetime')
+    );
+});
 
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_datetime" name="test_datetime" type="datetime-local" value="2020-01-20T15:00:12"/>',
-            $this->html->datetime('test_datetime')
-        );
-    }
+it('can create a datetime input with invalid date', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_datetime" name="test_datetime" type="datetime-local" value="notadate"/>',
+        $this->html->datetime('test_datetime', 'notadate')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_datetime_input_with_invalid_date()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_datetime" name="test_datetime" type="datetime-local" value="notadate"/>',
-            $this->html->datetime('test_datetime', 'notadate')
-        );
-    }
+it('can create a time input', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="time">',
+        $this->html->time()
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_time_input()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="time">',
-            $this->html->time()
-        );
-    }
+it('can create a time input with blank value', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_time" name="test_time" type="time" value=""/>',
+        $this->html->time('test_time', '')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_time_input_with_blank_value()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_time" name="test_time" type="time" value=""/>',
-            $this->html->time('test_time', '')
-        );
-    }
+it('can create a time input with time string and format', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_time" name="test_time" type="time" value="11:30:00"/>',
+        $this->html->time('test_time', '11:30')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_time_input_with_time_string_and_format()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_time" name="test_time" type="time" value="11:30:00"/>',
-            $this->html->time('test_time', '11:30')
-        );
-    }
+it('can create a time input with string and format', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_time" name="test_time" type="time" value="23:33:32"/>',
+        $this->html->time('test_time', '2017-09-04T23:33:32')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_time_input_with_string_and_format()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_time" name="test_time" type="time" value="23:33:32"/>',
-            $this->html->time('test_time', '2017-09-04T23:33:32')
-        );
-    }
+it('can create a time input with invalid time', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input id="test_time" name="test_time" type="time" value="timeoclock"/>',
+        $this->html->time('test_time', 'timeoclock')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_time_input_with_invalid_time()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input id="test_time" name="test_time" type="time" value="timeoclock"/>',
-            $this->html->time('test_time', 'timeoclock')
-        );
-    }
+it('can create a time input with model time', function () {
+    $this->html->model(['time' => '2017-09-04T23:33:32']);
 
-    /** @test */
-    public function it_can_create_a_time_input_with_model_time()
-    {
-        $this->html->model(['time' => '2017-09-04T23:33:32']);
+    assertHtmlStringEqualsHtmlString(
+        '<input id="time" name="time" type="time" value="23:33:32"/>',
+        $this->html->time('time')
+    );
+});
 
-        assertHtmlStringEqualsHtmlString(
-            '<input id="time" name="time" type="time" value="23:33:32"/>',
-            $this->html->time('time')
-        );
-    }
+it('can create a range input', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="range">',
+        $this->html->range()
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_range_input()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="range">',
-            $this->html->range()
-        );
-    }
+it('can create a range input with min max', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="range" name="test" id="test" value="0" min="0" max="100">',
+        $this->html->range('test', '0', '0', '100')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_range_input_with_min_max()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="range" name="test" id="test" value="0" min="0" max="100">',
-            $this->html->range('test', '0', '0', '100')
-        );
-    }
+it('can create a range input with min max step', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="range" name="test" id="test" value="0" min="0" max="100" step="10">',
+        $this->html->range('test', '0', '0', '100', '10')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_range_input_with_min_max_step()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="range" name="test" id="test" value="0" min="0" max="100" step="10">',
-            $this->html->range('test', '0', '0', '100', '10')
-        );
-    }
+it('can create a range input with max step', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="range" name="test" id="test" value="30" max="100" step="10">',
+        $this->html->range('test', '30', null, '100', '10')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_range_input_with_max_step()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="range" name="test" id="test" value="30" max="100" step="10">',
-            $this->html->range('test', '30', null, '100', '10')
-        );
-    }
+it('can create a number input', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="number">',
+        $this->html->number()
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_number_input()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="number">',
-            $this->html->number()
-        );
-    }
+it('can create a number input with min max', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="number" name="test" id="test" value="0" min="0" max="100">',
+        $this->html->number('test', '0', '0', '100')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_number_input_with_min_max()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="number" name="test" id="test" value="0" min="0" max="100">',
-            $this->html->number('test', '0', '0', '100')
-        );
-    }
+it('can create a number input with min max step', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="number" name="test" id="test" value="0" min="0" max="100" step="10">',
+        $this->html->number('test', '0', '0', '100', '10')
+    );
+});
 
-    /** @test */
-    public function it_can_create_a_number_input_with_min_max_step()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="number" name="test" id="test" value="0" min="0" max="100" step="10">',
-            $this->html->number('test', '0', '0', '100', '10')
-        );
-    }
-
-    /** @test */
-    public function it_can_create_a_number_input_with_max_step()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="number" name="test" id="test" value="30" max="100" step="10">',
-            $this->html->number('test', '30', null, '100', '10')
-        );
-    }
-}
+it('can create a number input with max step', function () {
+    assertHtmlStringEqualsHtmlString(
+        '<input type="number" name="test" id="test" value="30" max="100" step="10">',
+        $this->html->number('test', '30', null, '100', '10')
+    );
+});

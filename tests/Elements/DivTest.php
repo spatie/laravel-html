68eb9001fcd394f0f47eceef8aff624ff0b7db0c
Alex Manase <malexandru699@gmail.com>
2023-01-19 17:43:46 +0200

    refactor: DivTest

--- tests/Elements/DivTest.php
+++ tests/Elements/DivTest.php
@@ -1,18 +1,9 @@
 <?php
 
-namespace Spatie\Html\Test\Elements;
-
 use Spatie\Html\Elements\Div;
-use Spatie\Html\Test\TestCase;
 
-class DivTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_div()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<div></div>',
-            Div::create()
-        );
-    }
-}
+it('can create a div')
+    ->assertHtmlStringEqualsHtmlString(
+        '<div></div>',
+        Div::create()
+    );

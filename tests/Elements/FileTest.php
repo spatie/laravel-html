07b1559a2892927557c6b00d17bd6bee565ff718
Alex Manase <malexandru699@gmail.com>
2023-01-19 17:52:40 +0200

    refactor: FileTest

--- tests/Elements/FileTest.php
+++ tests/Elements/FileTest.php
@@ -1,123 +1,82 @@
 <?php
 
-namespace Spatie\Html\Test\Elements;
-
 use Spatie\Html\Elements\File;
-use Spatie\Html\Test\TestCase;
 
-class FileTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_file()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file">',
-            File::create()
-        );
-    }
+it('can create a file')
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file">',
+        File::create()
+    );
 
-    /** @test */
-    public function it_can_create_an_autofocused_file()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" autofocus>',
-            File::create()->autofocus()
-        );
-    }
+it('can create an autofocused file')
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" autofocus>',
+        File::create()->autofocus()
+    );
 
-    /** @test */
-    public function it_can_create_a_file_that_has_autofocus_when_passing_true()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" autofocus>',
-            File::create()->autofocus(true)
-        );
-    }
+it('can create a file that has autofocus when passing true')
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" autofocus>',
+        File::create()->autofocus(true)
+    );
 
-    /** @test */
-    public function it_wont_create_a_file_that_has_autofocus_when_passing_false()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file">',
-            File::create()->autofocus(false)
-        );
-    }
+it("won't create a file that has autofocus when passing false")
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file">',
+        File::create()->autofocus(false)
+    );
 
-    /** @test */
-    public function it_can_create_an_required_file()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" required>',
-            File::create()->required()
-        );
-    }
+it('can create an required file')
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" required>',
+        File::create()->required()
+    );
 
-    /** @test */
-    public function it_can_create_a_file_with_a_name()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" name="file">',
-            File::create()->name('file')
-        );
-    }
+it('can create a file with a name')
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" name="file">',
+        File::create()->name('file')
+    );
 
-    /** @test */
-    public function it_can_create_a_file_with_accept_audio()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" accept="audio/*">',
-            File::create()->accept(File::ACCEPT_AUDIO)
-        );
+it('can create a file with accept audio')
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" accept="audio/*">',
+        File::create()->accept(File::ACCEPT_AUDIO)
+    )
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" accept="audio/*">',
+        File::create()->acceptAudio()
+    );;
 
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" accept="audio/*">',
-            File::create()->acceptAudio()
-        );
-    }
+it('can create a file with accept video')
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" accept="video/*">',
+        File::create()->accept(File::ACCEPT_VIDEO)
+    )
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" accept="video/*">',
+        File::create()->acceptVideo()
+    );
 
-    /** @test */
-    public function it_can_create_a_file_with_accept_video()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" accept="video/*">',
-            File::create()->accept(File::ACCEPT_VIDEO)
-        );
+it('can create a file with accept image')
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" accept="image/*">',
+        File::create()->accept(File::ACCEPT_IMAGE)
+    )
 
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" accept="video/*">',
-            File::create()->acceptVideo()
-        );
-    }
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" accept="image/*">',
+        File::create()->acceptImage()
+    );
 
-    /** @test */
-    public function it_can_create_a_file_with_accept_image()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" accept="image/*">',
-            File::create()->accept(File::ACCEPT_IMAGE)
-        );
+it('can create a file with accept')
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" accept=".jpg">',
+        File::create()->accept('.jpg')
+    );
 
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" accept="image/*">',
-            File::create()->acceptImage()
-        );
-    }
-
-    /** @test */
-    public function it_can_create_a_file_with_accept()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" accept=".jpg">',
-            File::create()->accept('.jpg')
-        );
-    }
-
-    /** @test */
-    public function it_can_create_a_file_with_multiple()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<input type="file" multiple>',
-            File::create()->multiple()
-        );
-    }
-}
+it('can create a file with multiple')
+    ->assertHtmlStringEqualsHtmlString(
+        '<input type="file" multiple>',
+        File::create()->multiple()
+    );

2aaa736f741f626c436754e0a7754945666dcfec
Alex Manase <malexandru699@gmail.com>
2023-01-19 17:47:43 +0200

    refactor: FieldsetTest

--- tests/Elements/FieldsetTest.php
+++ tests/Elements/FieldsetTest.php
@@ -1,36 +1,21 @@
 <?php
 
-namespace Spatie\Html\Test\Elements;
-
 use Spatie\Html\Elements\Fieldset;
-use Spatie\Html\Test\TestCase;
 
-class FieldsetTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_fieldset()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<fieldset></fieldset>',
-            Fieldset::create()
-        );
-    }
+it('can create a fieldset')
+    ->assertHtmlStringEqualsHtmlString(
+        '<fieldset></fieldset>',
+        Fieldset::create()
+    );
 
-    /** @test */
-    public function it_can_add_a_legend_to_the_fieldset()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<fieldset><legend>Legend</legend></fieldset>',
-            Fieldset::create()->legend('Legend')
-        );
-    }
+it('can add a legend to the fieldset')
+    ->assertHtmlStringEqualsHtmlString(
+        '<fieldset><legend>Legend</legend></fieldset>',
+        Fieldset::create()->legend('Legend')
+    );
 
-    /** @test */
-    public function it_can_disable_a_fieldset()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<fieldset disabled></fieldset>',
-            Fieldset::create()->disabled()
-        );
-    }
-}
+it('can disable a fieldset')
+    ->assertHtmlStringEqualsHtmlString(
+        '<fieldset disabled></fieldset>',
+        Fieldset::create()->disabled()
+    );

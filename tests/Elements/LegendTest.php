1a34aa46d5c981d408c0c6257e5fe629ce0b49d6
Alex Manase <malexandru699@gmail.com>
2023-01-20 13:00:11 +0200

    refactor: LegendTest

--- tests/Elements/LegendTest.php
+++ tests/Elements/LegendTest.php
@@ -1,18 +1,9 @@
 <?php
 
-namespace Spatie\Html\Test\Elements;
-
 use Spatie\Html\Elements\Legend;
-use Spatie\Html\Test\TestCase;
 
-class LegendTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_legend()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<legend></legend>',
-            Legend::create()
-        );
-    }
-}
+it('can create a legend')
+    ->assertHtmlStringEqualsHtmlString(
+        '<legend></legend>',
+        Legend::create()
+    );

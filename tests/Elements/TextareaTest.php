a1fbb35d81af11145cf66c684ef8e94cb91750d0
Alex Manase <malexandru699@gmail.com>
2023-01-20 13:40:09 +0200

    refactor: TextareaTest

--- tests/Elements/TextareaTest.php
+++ tests/Elements/TextareaTest.php
@@ -1,153 +1,99 @@
 <?php
 
-namespace Spatie\Html\Test\Elements;
-
 use Spatie\Html\Elements\Textarea;
-use Spatie\Html\Test\TestCase;
 
-class TextareaTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_textarea()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea></textarea>',
-            Textarea::create()
-        );
-    }
+it('can create a textarea')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea></textarea>',
+        Textarea::create()
+    );
 
-    /** @test */
-    public function it_can_create_an_autofocused_textarea()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea autofocus></textarea>',
-            Textarea::create()->autofocus()
-        );
-    }
+it('can create an autofocused textarea')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea autofocus></textarea>',
+        Textarea::create()->autofocus()
+    );
 
-    /** @test */
-    public function it_can_create_a_textarea_with_a_placeholder()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea placeholder="Lorem ipsum"></textarea>',
-            Textarea::create()->placeholder('Lorem ipsum')
-        );
-    }
+it('can create a textarea with a placeholder')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea placeholder="Lorem ipsum"></textarea>',
+        Textarea::create()->placeholder('Lorem ipsum')
+    );
 
-    /** @test */
-    public function it_can_create_a_textarea_with_a_name()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea name="text"></textarea>',
-            Textarea::create()->name('text')
-        );
-    }
+it('can create a textarea with a name')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea name="text"></textarea>',
+        Textarea::create()->name('text')
+    );
 
-    /** @test */
-    public function it_can_create_a_required_textarea()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea required>My epic</textarea>',
-            Textarea::create()->value('My epic')->required()
-        );
-    }
+it('can create a required textarea')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea required>My epic</textarea>',
+        Textarea::create()->value('My epic')->required()
+    );
 
-    /** @test */
-    public function it_can_create_a_textarea_with_a_value()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea>My epic</textarea>',
-            Textarea::create()->value('My epic')
-        );
-    }
+it('can create a textarea with a value')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea>My epic</textarea>',
+        Textarea::create()->value('My epic')
+    );
 
-    /** @test */
-    public function it_can_create_a_textarea_that_is_required_when_passing_true()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea required>My epic</textarea>',
-            Textarea::create()->value('My epic')->required(true)
-        );
-    }
+it('can create a textarea that is required when passing true')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea required>My epic</textarea>',
+        Textarea::create()->value('My epic')->required(true)
+    );
 
-    /** @test */
-    public function it_wont_create_a_textarea_that_is_required_when_passing_false()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea>My epic</textarea>',
-            Textarea::create()->value('My epic')->required(false)
-        );
-    }
+it("won't create a textarea that is required when passing false")
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea>My epic</textarea>',
+        Textarea::create()->value('My epic')->required(false)
+    );
 
-    /** @test */
-    public function it_can_create_a_disabled_textarea()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea disabled>My epic</textarea>',
-            Textarea::create()->value('My epic')->disabled()
-        );
-    }
+it('can create a disabled textarea')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea disabled>My epic</textarea>',
+        Textarea::create()->value('My epic')->disabled()
+    );
 
-    /** @test */
-    public function it_can_create_a_textarea_that_is_disabled_when_passing_true()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea disabled>My epic</textarea>',
-            Textarea::create()->value('My epic')->disabled(true)
-        );
-    }
+it('can create a textarea that is disabled when passing true')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea disabled>My epic</textarea>',
+        Textarea::create()->value('My epic')->disabled(true)
+    );
 
-    /** @test */
-    public function it_wont_create_a_textarea_that_is_disabled_when_passing_false()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea>My epic</textarea>',
-            Textarea::create()->value('My epic')->disabled(false)
-        );
-    }
+it("won't create a textarea that is disabled when passing false")
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea>My epic</textarea>',
+        Textarea::create()->value('My epic')->disabled(false)
+    );
 
-    /** @test */
-    public function it_can_create_a_readonly_textarea()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea readonly>My epic</textarea>',
-            Textarea::create()->value('My epic')->isReadonly()
-        );
-    }
+it('can create a readonly textarea')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea readonly>My epic</textarea>',
+        Textarea::create()->value('My epic')->isReadonly()
+    );
 
-    /** @test */
-    public function it_can_create_a_textarea_that_is_readonly_when_passing_true()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea readonly>My epic</textarea>',
-            Textarea::create()->value('My epic')->isReadonly(true)
-        );
-    }
+it('can create a textarea that is readonly when passing true')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea readonly>My epic</textarea>',
+        Textarea::create()->value('My epic')->isReadonly(true)
+    );
 
-    /** @test */
-    public function it_wont_create_a_textarea_that_is_readonly_when_passing_false()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea>My epic</textarea>',
-            Textarea::create()->value('My epic')->isReadonly(false)
-        );
-    }
+it("won't create a textarea that is readonly when passing false")
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea>My epic</textarea>',
+        Textarea::create()->value('My epic')->isReadonly(false)
+    );
 
-    /** @test */
-    public function it_can_create_a_textarea_with_maxlength()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea maxlength="25">My epic</textarea>',
-            Textarea::create()->value('My epic')->maxlength(25)
-        );
-    }
+it('can create a textarea with maxlength')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea maxlength="25">My epic</textarea>',
+        Textarea::create()->value('My epic')->maxlength(25)
+    );
 
-    /** @test */
-    public function it_can_create_a_textarea_with_minlength()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<textarea minlength="25">My epic</textarea>',
-            Textarea::create()->value('My epic')->minlength(25)
-        );
-    }
-}
+it('can create a textarea with minlength')
+    ->assertHtmlStringEqualsHtmlString(
+        '<textarea minlength="25">My epic</textarea>',
+        Textarea::create()->value('My epic')->minlength(25)
+    );

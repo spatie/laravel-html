0cd20010ac6cb4297300fc3640a4f89347f82525
Alex Manase <malexandru699@gmail.com>
2023-01-20 13:17:03 +0200

    refactor: SelectTest

--- tests/Elements/SelectTest.php
+++ tests/Elements/SelectTest.php
@@ -1,152 +1,118 @@
 <?php
 
-namespace Spatie\Html\Test\Elements;
-
 use Spatie\Html\Elements\Select;
-use Spatie\Html\Test\TestCase;
 
-class SelectTest extends TestCase
-{
-    /** @test */
-    public function it_can_render_an_empty_version_itself()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select></select>',
-            Select::create()->render()
-        );
-    }
+it('can render an empty version itself')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select></select>',
+        Select::create()->render()
+    );
 
-    /** @test */
-    public function it_can_render_options()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select>
+it('can render options')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_can_have_a_value()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select>
+it('can have a value')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select>
                 <option value="value1">text1</option>
                 <option selected value="value2">text2</option>
             </select>',
-            Select::create()
-                ->options(['value1' => 'text1', 'value2' => 'text2'])
-                ->value('value2')
-                ->render()
-        );
-    }
+        Select::create()
+            ->options(['value1' => 'text1', 'value2' => 'text2'])
+            ->value('value2')
+            ->render()
+    );
 
-    /** @test */
-    public function it_can_have_a_placeholder_option()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select>
+it('can have a placeholder option')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select>
                 <option value="" selected="selected">Placeholder</option>
                 <option value="value1">text1</option>
                 <option value="value2">text2</option>
             </select>',
-            Select::create()
-                ->options(['value1' => 'text1', 'value2' => 'text2'])
-                ->placeholder('Placeholder')
-                ->render()
-        );
-    }
+        Select::create()
+            ->options(['value1' => 'text1', 'value2' => 'text2'])
+            ->placeholder('Placeholder')
+            ->render()
+    );
 
-    /** @test */
-    public function it_doesnt_select_the_placeholder_if_something_has_already_been_selected()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select>
+it("doesn't select the placeholder if something has already been selected")
+    ->assertHtmlStringEqualsHtmlString(
+        '<select>
                 <option value="">Placeholder</option>
                 <option value="value1" selected="selected">text1</option>
                 <option value="value2">text2</option>
             </select>',
-            Select::create()
-                ->options(['value1' => 'text1', 'value2' => 'text2'])
-                ->value('value1')
-                ->placeholder('Placeholder')
-                ->render()
-        );
-    }
+        Select::create()
+            ->options(['value1' => 'text1', 'value2' => 'text2'])
+            ->value('value1')
+            ->placeholder('Placeholder')
+            ->render()
+    );
 
-    /** @test */
-    public function it_can_have_a_multiple_option()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select multiple="multiple">
+it('can have a multiple option')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select multiple="multiple">
                 <option value="value1">text1</option>
                 <option value="value2">text2</option>
                 <option value="value3">text3</option>
             </select>',
-            Select::create()
-                ->options(['value1' => 'text1', 'value2' => 'text2', 'value3' => 'text3'])
-                ->multiple()
-                ->render()
-        );
-    }
+        Select::create()
+            ->options(['value1' => 'text1', 'value2' => 'text2', 'value3' => 'text3'])
+            ->multiple()
+            ->render()
+    );
 
-    /** @test */
-    public function it_can_convert_multiple_select_name()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select multiple="multiple" name="foo[]">
+it('can convert multiple select name')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select multiple="multiple" name="foo[]">
                 <option value="value1">text1</option>
                 <option value="value2">text2</option>
                 <option value="value3">text3</option>
             </select>',
-            Select::create()
-                  ->name('foo')
-                  ->options(['value1' => 'text1', 'value2' => 'text2', 'value3' => 'text3'])
-                  ->multiple()
-                  ->render()
-        );
-    }
+        Select::create()
+            ->name('foo')
+            ->options(['value1' => 'text1', 'value2' => 'text2', 'value3' => 'text3'])
+            ->multiple()
+            ->render()
+    );
 
-    /** @test */
-    public function it_can_have_multiple_values()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select multiple="multiple">
+it('can have multiple values')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select multiple="multiple">
                 <option value="value1" selected="selected">text1</option>
                 <option value="value2">text2</option>
                 <option value="value3" selected="selected">text3</option>
             </select>',
-            Select::create()
-                  ->options(['value1' => 'text1', 'value2' => 'text2', 'value3' => 'text3'])
-                  ->value(['value1', 'value3'])
-                  ->multiple()
-                  ->render()
-        );
-    }
+        Select::create()
+            ->options(['value1' => 'text1', 'value2' => 'text2', 'value3' => 'text3'])
+            ->value(['value1', 'value3'])
+            ->multiple()
+            ->render()
+    );
 
-    /** @test */
-    public function it_can_have_one_multiple_value()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select multiple="multiple">
+it('can have one multiple value')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select multiple="multiple">
                 <option value="value1">text1</option>
                 <option value="value2">text2</option>
                 <option value="value3" selected="selected">text3</option>
             </select>',
-            Select::create()
-                  ->options(['value1' => 'text1', 'value2' => 'text2', 'value3' => 'text3'])
-                  ->value('value3')
-                  ->multiple()
-                  ->render()
-        );
-    }
+        Select::create()
+            ->options(['value1' => 'text1', 'value2' => 'text2', 'value3' => 'text3'])
+            ->value('value3')
+            ->multiple()
+            ->render()
+    );
 
-    /** @test */
-    public function it_can_have_an_optgroup()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select>
+it('can have an optgroup')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select>
                 <optgroup label="Cats">
                     <option value="leopard">Leopard</option>
                 </optgroup>
@@ -154,17 +120,14 @@ class SelectTest extends TestCase
                     <option value="spaniel">Spaniel</option>
                 </optgroup>
             </select>',
-            Select::create()
-                ->options(['Cats' => ['leopard' => 'Leopard'], 'Dogs' => ['spaniel' => 'Spaniel']])
-                ->render()
-        );
-    }
+        Select::create()
+            ->options(['Cats' => ['leopard' => 'Leopard'], 'Dogs' => ['spaniel' => 'Spaniel']])
+            ->render()
+    );
 
-    /** @test */
-    public function it_can_select_an_item_in_an_optgroup()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select>
+it('can select an item in an optgroup')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select>
                 <optgroup label="Cats">
                     <option value="leopard" selected="selected">Leopard</option>
                 </optgroup>
@@ -172,142 +135,104 @@ class SelectTest extends TestCase
                     <option value="spaniel">Spaniel</option>
                 </optgroup>
             </select>',
-            Select::create()
-                ->options(['Cats' => ['leopard' => 'Leopard'], 'Dogs' => ['spaniel' => 'Spaniel']])
-                ->value('leopard')
-                ->render()
-        );
-    }
+        Select::create()
+            ->options(['Cats' => ['leopard' => 'Leopard'], 'Dogs' => ['spaniel' => 'Spaniel']])
+            ->value('leopard')
+            ->render()
+    );
 
-    /** @test */
-    public function it_can_create_a_disabled_select()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select disabled>
+it('can create a disabled select')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select disabled>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->disabled()->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->disabled()->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_can_create_a_select_that_is_disabled_when_passing_true()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select disabled>
+it('can create a select that is disabled when passing true')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select disabled>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->disabled(true)->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->disabled(true)->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_wont_create_a_select_that_is_disabled_when_passing_false()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select>
+it("won't create a select that is disabled when passing false")
+    ->assertHtmlStringEqualsHtmlString(
+        '<select>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->disabled(false)->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->disabled(false)->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_can_create_a_select_that_has_autofocus()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select autofocus>
+it('can create a select that has autofocus')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select autofocus>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->autofocus()->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->autofocus()->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_can_create_a_select_that_has_autofocus_when_passing_true()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select autofocus>
+it('can create a select that has autofocus when passing true')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select autofocus>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->autofocus(true)->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->autofocus(true)->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_wont_create_a_select_that_has_autofocus_when_passing_false()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select>
+it("won't create a select that has autofocus when passing false")
+    ->assertHtmlStringEqualsHtmlString(
+        '<select>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->autofocus(false)->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->autofocus(false)->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_can_create_a_select_that_is_required()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select required>
+it('can create a select that is required')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select required>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->required()->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->required()->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_can_create_a_select_that_is_required_when_passing_true()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select required>
+it('can create a select that is required when passing true')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select required>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->required(true)->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->required(true)->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_wont_create_a_select_that_is_required_when_passing_false()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select>
+it("won't create a select that is required when passing false")
+    ->assertHtmlStringEqualsHtmlString(
+        '<select>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->required(false)->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->required(false)->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_can_create_a_select_that_is_readonly()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select readonly>
+it('can create a select that is readonly')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select readonly>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->isReadonly()->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->isReadonly()->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_can_create_a_select_that_is_readonly_when_passing_true()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select readonly>
+it('can create a select that is readonly when passing true')
+    ->assertHtmlStringEqualsHtmlString(
+        '<select readonly>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->isReadonly(true)->options(['value1' => 'text1'])->render()
-        );
-    }
+        Select::create()->isReadonly(true)->options(['value1' => 'text1'])->render()
+    );
 
-    /** @test */
-    public function it_wont_create_a_select_that_is_readonly_when_passing_false()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<select>
+it("won't create a select that is readonly when passing false")
+    ->assertHtmlStringEqualsHtmlString(
+        '<select>
                 <option value="value1">text1</option>
             </select>',
-            Select::create()->isReadonly(false)->options(['value1' => 'text1'])->render()
-        );
-    }
-}
+        Select::create()->isReadonly(false)->options(['value1' => 'text1'])->render()
+    );

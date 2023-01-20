196879f411a42e206216bee1cccd83ecf34e6419
Alex Manase <malexandru699@gmail.com>
2023-01-20 13:04:24 +0200

    refactor: OptionTest

--- tests/Elements/OptionTest.php
+++ tests/Elements/OptionTest.php
@@ -1,68 +1,43 @@
 <?php
 
-namespace Spatie\Html\Test\Elements;
-
 use Spatie\Html\Elements\Option;
-use Spatie\Html\Test\TestCase;
 
-class OptionTest extends TestCase
-{
-    /** @test */
-    public function it_can_render_an_empty_version_itself()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<option></option>',
-            Option::create()->render()
-        );
-    }
+it('can render an empty version itself')
+    ->assertHtmlStringEqualsHtmlString(
+        '<option></option>',
+        Option::create()->render()
+    );
 
-    /** @test */
-    public function it_can_render_itself_with_a_text_and_a_value()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<option value="0">Choose...</option>',
-            Option::create()->value('0')->text('Choose...')->render()
-        );
-    }
+it('can render itself with a text and a value')
+    ->assertHtmlStringEqualsHtmlString(
+        '<option value="0">Choose...</option>',
+        Option::create()->value('0')->text('Choose...')->render()
+    );
 
-    /** @test */
-    public function it_can_render_itself_in_a_selected_state()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<option selected value="0">Choose...</option>',
-            Option::create()->value('0')->text('Choose...')->selected('0')
-        );
-    }
+it('can render itself in a selected state')
+    ->assertHtmlStringEqualsHtmlString(
+        '<option selected value="0">Choose...</option>',
+        Option::create()->value('0')->text('Choose...')->selected('0')
+    );
 
-    /** @test */
-    public function it_can_unselect_itself()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<option value="0">Choose...</option>',
-            Option::create()->value('0')->text('Choose...')->selected('0')->unselected()
-        );
-    }
+it('can unselect itself')
+    ->assertHtmlStringEqualsHtmlString(
+        '<option value="0">Choose...</option>',
+        Option::create()->value('0')->text('Choose...')->selected('0')->unselected()
+    );
 
-    /** @test */
-    public function it_can_conditionally_select_itself()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<option selected value="0">Choose...</option>',
-            Option::create()->value('0')->text('Choose...')->selectedIf(true)
-        );
+it('can conditionally select itself')
+    ->assertHtmlStringEqualsHtmlString(
+        '<option selected value="0">Choose...</option>',
+        Option::create()->value('0')->text('Choose...')->selectedIf(true)
+    )
+    ->assertHtmlStringEqualsHtmlString(
+        '<option value="0">Choose...</option>',
+        Option::create()->value('0')->text('Choose...')->selectedIf(false)
+    );
 
-        assertHtmlStringEqualsHtmlString(
-            '<option value="0">Choose...</option>',
-            Option::create()->value('0')->text('Choose...')->selectedIf(false)
-        );
-    }
-
-    /** @test */
-    public function it_can_disable_an_optgroup()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<option disabled></option>',
-            Option::create()->disabled()
-        );
-    }
-}
+it('can disable an optgroup')
+    ->assertHtmlStringEqualsHtmlString(
+        '<option disabled></option>',
+        Option::create()->disabled()
+    );

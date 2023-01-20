8e5b28cb898230b9b90a56bc3a4649805babdd0a
Alex Manase <malexandru699@gmail.com>
2023-01-20 12:58:33 +0200

    refactor: ITest

--- tests/Elements/ITest.php
+++ tests/Elements/ITest.php
@@ -1,18 +1,9 @@
 <?php
 
-namespace Spatie\Html\Test\Elements;
-
 use Spatie\Html\Elements\I;
-use Spatie\Html\Test\TestCase;
 
-class ITest extends TestCase
-{
-    /** @test */
-    public function it_can_create_an_i_element()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<i></i>',
-            I::create()
-        );
-    }
-}
+it('it can create an i element')
+    ->assertHtmlStringEqualsHtmlString(
+        '<i></i>',
+        I::create()
+    );

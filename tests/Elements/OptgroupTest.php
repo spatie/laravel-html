6e6485e2099f4cc77195091203df728086a6aedc
Alex Manase <malexandru699@gmail.com>
2023-01-20 13:01:29 +0200

    refactor: OptgroupTest

--- tests/Elements/OptgroupTest.php
+++ tests/Elements/OptgroupTest.php
@@ -1,36 +1,21 @@
 <?php
 
-namespace Spatie\Html\Test\Elements;
-
 use Spatie\Html\Elements\Optgroup;
-use Spatie\Html\Test\TestCase;
 
-class OptgroupTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_an_optgroup()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<optgroup></optgroup>',
-            Optgroup::create()
-        );
-    }
+it('can create an optgroup')
+    ->assertHtmlStringEqualsHtmlString(
+        '<optgroup></optgroup>',
+        Optgroup::create()
+    );
 
-    /** @test */
-    public function it_can_create_an_element_with_a_label()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<optgroup label="Cats"></optgroup>',
-            Optgroup::create()->label('Cats')
-        );
-    }
+it('can create an element with a label')
+    ->assertHtmlStringEqualsHtmlString(
+        '<optgroup label="Cats"></optgroup>',
+        Optgroup::create()->label('Cats')
+    );
 
-    /** @test */
-    public function it_can_disable_an_optgroup()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<optgroup disabled></optgroup>',
-            Optgroup::create()->disabled()
-        );
-    }
-}
+it('can disable an optgroup')
+    ->assertHtmlStringEqualsHtmlString(
+        '<optgroup disabled></optgroup>',
+        Optgroup::create()->disabled()
+    );

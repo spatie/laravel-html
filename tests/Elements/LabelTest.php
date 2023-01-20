222aeae0ef184eb0d4f655cabb4bb3b9c03f2cf1
Alex Manase <malexandru699@gmail.com>
2023-01-20 12:59:25 +0200

    refactor: LabelTest

--- tests/Elements/LabelTest.php
+++ tests/Elements/LabelTest.php
@@ -1,27 +1,15 @@
 <?php
 
-namespace Spatie\Html\Test\Elements;
-
 use Spatie\Html\Elements\Label;
-use Spatie\Html\Test\TestCase;
 
-class LabelTest extends TestCase
-{
-    /** @test */
-    public function it_can_create_a_label()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<label></label>',
-            Label::create()
-        );
-    }
+it('can create a label')
+    ->assertHtmlStringEqualsHtmlString(
+        '<label></label>',
+        Label::create()
+    );
 
-    /** @test */
-    public function it_can_create_a_label_with_a_custom_for_attribute()
-    {
-        assertHtmlStringEqualsHtmlString(
-            '<label for="some_input_id"></label>',
-            Label::create()->for('some_input_id')
-        );
-    }
-}
+it('can create a label with a custom for attribute')
+    ->assertHtmlStringEqualsHtmlString(
+        '<label for="some_input_id"></label>',
+        Label::create()->for('some_input_id')
+    );
